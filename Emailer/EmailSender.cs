c97397d43d6b577ac95b066250045f711ee0efc3
inkey <phelimmbrady@gmail.com>
2017-05-04 19:21:33 +0100

    Moved email sender to own class away from program file

---
+++ Emailer/EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emailer
+{
+  public  class EmailSender
+    {
+        //This is the most common dependency injection.
+        //When a class requires an instance of a dependency to work, we can supply that dependency through the class’s constructor. 
+
+        private IEmailService _iEmailService;
+        //the implementation of the constructor is very simple 
+        public EmailSender(IEmailService _mailService)
+        {
+            this._iEmailService = _mailService;
+        }
+        public void Notification(EmailInfo ObjEmailInfo)
+        {
+            _iEmailService.sendMail(ObjEmailInfo);
+        }
+    }
+}
